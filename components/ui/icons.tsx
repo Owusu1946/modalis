6db0eeda5d35c63f7317c078d68dbc1167be52b5
Owusu Kenneth <owusukenneth77@gmail.com>
2025-09-25 08:03:55 +0200

    update docs

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { cn } from '@/lib/utils'
 import { useEffect, useRef, useState } from 'react'
+
+import { cn } from '@/lib/utils'
 
 function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
